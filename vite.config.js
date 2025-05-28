37d92c76c6893dd97d5e460cb70d82f24887d94a
Madiyar <madiarduysenbay@gmail.com>
2025-05-28 13:17:43 +0500

    f

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  
   build: {
     outDir: 'dist',
   },
   server: {
-    host: '0.0.0.0',
-    port: 443,
     allowedHosts: ['...'],
   },
   envPrefix: 'VITE_',
